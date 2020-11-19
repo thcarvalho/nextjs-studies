1a2ae6c309e642fd8d1d47d42ddf983a6277a587
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-19 11:11:31 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Brabissimo</title>
+      </Head>
+
+      <main>
+        <p>Xablau</p>
+      </main>
+    </div>
+  )
+}
